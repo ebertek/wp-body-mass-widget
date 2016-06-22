7598aca66cd9997d0007e14e2854bc111e5155ec
David Ebert <ebertek@gmail.com>
2016-06-22 17:01:32 +0200

    Add all BMI categories

--- body-mass-widget.php
+++ body-mass-widget.php
@@ -2,9 +2,9 @@
 /*
 Plugin Name: Body Mass Widget
 Plugin URI: https://biombodigital.com
-Description: Adds a widget that shows a BMI calculater. You can change all the labels and it is totally free.
-Author: Michelle Salabert
-Version: 1.0
+Description: Adds a widget that shows a BMI calculator. You can change all the labels and it is totally free.
+Author: Salabert + Ebert
+Version: 1.1
 Author URI: http://biombodigital.com
 
     Copyright 2014 Michelle Salabert (michelle@biombodigital.com)
@@ -21,6 +21,9 @@ Author URI: http://biombodigital.com
 
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Modified 2016 @ebertek
+
 */
 
 add_action( 'widgets_init', 'bodycalculater_register_widgets' );
@@ -42,8 +45,8 @@ function bodycalculater_register_plugin_styles() {
 class BodyMass extends WP_Widget {
 
 	function BodyMass() {
-		$widget_ops = array('description' => 'A bmi calculater widget by michellesalabert.com');
-		$title ='Body Mass Calculater';
+		$widget_ops = array('description' => 'A bmi calculator widget by michellesalabert.com');
+		$title ='Body Mass Calculator';
 		parent::__construct( false, $title, $widget_ops );
 	}
 
@@ -58,11 +61,14 @@ function widget( $args, $instance ) {
 		$error = $instance['error'];
 		$weight = $instance ['weight'];
 		$height = $instance ['height'];
-		$underweight = $instance ['underweight'];
-		$normal = $instance ['normal'];
-		$overweight = $instance ['overweight'];
-		$obese = $instance ['obese'];
-		$link = $instance['link'];
+		$bmikat1 = $instance ['bmikat1'];
+		$bmikat2 = $instance ['bmikat2'];
+		$bmikat3 = $instance ['bmikat3'];
+		$bmikat4 = $instance ['bmikat4'];
+		$bmikat5 = $instance ['bmikat5'];
+		$bmikat6 = $instance ['bmikat6'];
+		$bmikat7 = $instance ['bmikat7'];
+		$bmikat8 = $instance ['bmikat8'];
 		$reset = $instance['reset'];
 
 		echo $before_widget; ?>
@@ -86,17 +92,12 @@ function widget( $args, $instance ) {
 			<tr><td><label for="height"><?php echo $height; ?>:</label></td><td><input type="text" name="height" id="height" /><span>cm</span></td></tr>
 
 			</table>
-
+<div style="margin: 0 auto; width: 200px;">
 			<input id="submit" onclick="bodymass_calculate()" type="button" value="<?php echo $calculate; ?>" />
 
 			<input id="reset" onclick="resetform()" type="button" value="<?php echo $reset; ?>" />
-
+</div>
 			<div id="bm_result"></div>
-
-			<?php if($link == 'on') { ?>
-
-					<a class="poweredby" href="http://michellesalabert.com" target="_blank">Powered by Michelle Salabert</a>
-			<?php } ?>
 
 		</div>
 
@@ -110,18 +111,26 @@ function widget( $args, $instance ) {
 		var bm = weight/(height*height);
 		var msg = '';
 
-		if(bm < 18.5) {
-			 msg = '<?php echo $underweight; ?>'; }
-		else if(bm > 18.5 && bm < 25) {
-			 msg = '<?php echo $normal; ?>'; }
-		else if(bm > 25 && bm < 30) {
-			 msg = '<?php echo $overweight; ?>'; }
-		else if(bm > 30) {
-			 msg = '<?php echo $obese; ?>'; }
-
-		if(weight > 0 && height > 0 && weight != null && height != null) {
-		bm_result.innerHTML = '<p><?php echo $yourbm; ?><strong>' + bm.toFixed(2) + '</strong></p><p><strong>' + msg + '</strong></p>'; } else {
-		alert('<?php echo $error; ?>');
+if(bm < 15) {
+  msg = '<?php echo $bmikat1; ?>';
+} else if(bm >= 15 && bm < 16) {
+  msg = '<?php echo $bmikat2; ?>';
+} else if(bm >= 16 && bm < 18.5) {
+  msg = '<?php echo $bmikat3; ?>';
+} else if(bm >= 18.5 && bm < 25) {
+  msg = '<?php echo $bmikat4; ?>';
+} else if(bm >= 25 && bm < 30) {
+  msg = '<?php echo $bmikat5; ?>';
+} else if(bm >= 30 && bm < 35) {
+  msg = '<?php echo $bmikat6; ?>';
+} else if(bm >= 35 && bm < 40) {
+  msg = '<?php echo $bmikat7; ?>';
+} else if(bm >= 40) {
+  msg = '<?php echo $bmikat8; ?>';
+}
+		if(weight > 0 && height > 0 && weight != null && height != null && weight < 635 && height < 272) {
+		bm_result.innerHTML = '<p><?php echo $yourbm; ?> <strong>' + bm.toFixed(2) + '</strong></p><p><strong>' + msg + '</strong></p>'; } else {
+		bm_result.innerHTML = '<p><?php echo $error; ?></p>';
 	}
 }
 		<!--Reset button Javacript code-->
@@ -145,14 +154,17 @@ function update( $new_instance, $old_instance ) {
 		$instance['border'] = $new_instance['border'];
 		$instance['calculate'] = $new_instance['calculate'];
 		$instance['yourbm'] = $new_instance['yourbm'];
-		$instance ['underweight'] = $new_instance ['underweight'];
-		$instance ['normal'] = $new_instance ['normal'];
-		$instance ['overweight'] = $new_instance ['overweight'];
-		$instance ['obese'] = $new_instance ['obese'];
+		$instance ['bmikat1'] = $new_instance ['bmikat1'];
+		$instance ['bmikat2'] = $new_instance ['bmikat2'];
+		$instance ['bmikat3'] = $new_instance ['bmikat3'];
+		$instance ['bmikat4'] = $new_instance ['bmikat4'];
+		$instance ['bmikat5'] = $new_instance ['bmikat5'];
+		$instance ['bmikat6'] = $new_instance ['bmikat6'];
+		$instance ['bmikat7'] = $new_instance ['bmikat7'];
+		$instance ['bmikat8'] = $new_instance ['bmikat8'];
 		$instance ['height'] = $new_instance ['height'];
 		$instance ['weight'] = $new_instance ['weight'];
 		$instance['error'] = $new_instance['error'];
-		$instance['link'] = $new_instance['link'];
 $instance['reset'] = $new_instance['reset'];
 
 		return $instance;
@@ -165,15 +177,18 @@ $instance['reset'] = $new_instance['reset'];
 							 'calculate' => 'Calculate',
 							 'reset' => 'Reset',
 							 'yourbm' => 'Your BMI is = ',
-							 'underweight' => 'Underweight',
-							 'normal' => 'Normal',
-							 'overweight' => 'Overweight',
-							 'obese' => 'Obese',
+							 'bmikat1' => 'Very severely underweight',
+							 'bmikat2' => 'Severely underweight',
+							 'bmikat3' => 'Underweight',
+							 'bmikat4' => 'Normal (healthy weight)',
+							 'bmikat5' => 'Overweight',
+							 'bmikat6' => 'Obese Class I (Moderately obese)',
+							 'bmikat7' => 'Obese Class II (Severely obese)',
+							 'bmikat8' => 'Obese Class III (Very severely obese)',
 							 'weight' => 'Weight',
 							 'height' => 'Height',
 							 'error' => 'Please enter valid information!',
-							 'border' => '#c5c8c4',
-							 'link' => '' );
+							 'border' => '#c5c8c4' );
 		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
 
 		<p>
@@ -190,26 +205,38 @@ $instance['reset'] = $new_instance['reset'];
 			<label for="<?php echo $this->get_field_id( 'yourbm' ); ?>">Your BMI</label><br/>
 			<input id="<?php echo $this->get_field_id('yourbm' ); ?>" name="<?php echo $this->get_field_name('yourbm'); ?>" value="<?php echo $instance['yourbm']; ?>" class="widefat" type="text" />
 			</p>
-
-		<p>
-			<label for="<?php echo $this->get_field_id( 'underweight' ); ?>">Underweight</label><br/>
-			<input id="<?php echo $this->get_field_id('underweight' ); ?>" name="<?php echo $this->get_field_name('underweight'); ?>" value="<?php echo $instance['underweight']; ?>" class="widefat" type="text" />
-			</p>
-
-		 <p>
-			<label for="<?php echo $this->get_field_id( 'normal' ); ?>">Normal</label><br/>
-			<input id="<?php echo $this->get_field_id('normal' ); ?>" name="<?php echo $this->get_field_name('normal'); ?>" value="<?php echo $instance['normal']; ?>" class="widefat" type="text" />
-			</p>
-
-			<p>
-			<label for="<?php echo $this->get_field_id( 'overweight' ); ?>">Overweight</label><br/>
-			<input id="<?php echo $this->get_field_id('overweight' ); ?>" name="<?php echo $this->get_field_name('overweight'); ?>" value="<?php echo $instance['overweight']; ?>" class="widefat" type="text" />
-			</p>
-
-			<p>
-			<label for="<?php echo $this->get_field_id( 'obese' ); ?>">Obese</label><br/>
-			<input id="<?php echo $this->get_field_id('obese' ); ?>" name="<?php echo $this->get_field_name('obese'); ?>" value="<?php echo $instance['obese']; ?>" class="widefat" type="text" />
-			</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat1' ); ?>">Very severely underweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat1' ); ?>" name="<?php echo $this->get_field_name('bmikat1'); ?>" value="<?php echo $instance['bmikat1']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat2' ); ?>">Severely underweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat2' ); ?>" name="<?php echo $this->get_field_name('bmikat2'); ?>" value="<?php echo $instance['bmikat2']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat3' ); ?>">Underweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat3' ); ?>" name="<?php echo $this->get_field_name('bmikat3'); ?>" value="<?php echo $instance['bmikat3']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat4' ); ?>">Normal</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat4' ); ?>" name="<?php echo $this->get_field_name('bmikat4'); ?>" value="<?php echo $instance['bmikat4']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat5' ); ?>">Overweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat5' ); ?>" name="<?php echo $this->get_field_name('bmikat5'); ?>" value="<?php echo $instance['bmikat5']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat6' ); ?>">Moderately overweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat6' ); ?>" name="<?php echo $this->get_field_name('bmikat6'); ?>" value="<?php echo $instance['bmikat6']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat7' ); ?>">Severely overweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat7' ); ?>" name="<?php echo $this->get_field_name('bmikat7'); ?>" value="<?php echo $instance['bmikat7']; ?>" class="widefat" type="text" />
+</p>
+<p>
+  <label for="<?php echo $this->get_field_id( 'bmikat8' ); ?>">Very severely overweight</label><br/>
+  <input id="<?php echo $this->get_field_id('bmikat8' ); ?>" name="<?php echo $this->get_field_name('bmikat8'); ?>" value="<?php echo $instance['bmikat8']; ?>" class="widefat" type="text" />
+</p>
 
 		<p>
 			<label for="<?php echo $this->get_field_id( 'weight' ); ?>">Weight</label><br/>
@@ -236,8 +263,5 @@ $instance['reset'] = $new_instance['reset'];
 			<input id="<?php echo $this->get_field_id( 'reset' ); ?>" name="<?php echo $this->get_field_name( 'reset' ); ?>" value="<?php echo $instance['reset']; ?>" class="widefat" type="text" /></label>
 		</p>
 
-		<p>
-			<label for="<?php echo $this->get_field_id( 'link' ); ?>">Support me! Link to michellesalabert.com <input id="<?php echo $this->get_field_id( 'link' ); ?>" name="<?php echo $this->get_field_name( 'link' ); ?>" class="checkbox" type="checkbox" <?php checked($instance['link'], 'on'); ?> /></label>
-		</p> 
 <?php }
 }?>
